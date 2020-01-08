499391f5fbe682abf634a9a741b6e04298b7505d
ajapon88 <ajapon88@gmail.com>
2020-01-08 13:23:35 +0900

    use ssh key secrets

--- genarate-slave-ssh-key.sh
+++ genarate-slave-ssh-key.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-mkdir -p ./jenkins_home/.ssh
-ssh-keygen -t rsa -C "" -N "" -f ./jenkins_home/.ssh/id_rsa
-echo "JENKINS_SLAVE_SSH_PUBKEY=$(cat ./jenkins_home/.ssh/id_rsa.pub)">>./.env
+mkdir -p .ssh
+ssh-keygen -t rsa -C "" -N "" -f .ssh/id_rsa
+echo "JENKINS_SLAVE_SSH_PUBKEY=$(cat .ssh/id_rsa.pub)">>./.env
