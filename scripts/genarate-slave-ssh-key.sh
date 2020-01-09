2c7d4e809f415a76d8bb96659cdbeec7968de509
ajapon88 <ajapon88@gmail.com>
2020-01-10 00:29:20 +0900

    generate-slave-ssh-key script suport regenerate

--- scripts/genarate-slave-ssh-key.sh
+++ scripts/genarate-slave-ssh-key.sh
@@ -4,4 +4,8 @@ cd "$(dirname "${0}")/../"
 
 mkdir -p .ssh
 ssh-keygen -t rsa -C "" -N "" -f .ssh/id_rsa
-echo "JENKINS_SLAVE_SSH_PUBKEY=$(cat .ssh/id_rsa.pub)">>./.env
+if ! grep -e "^JENKINS_SLAVE_SSH_PUBKEY=" ./.env >/dev/null 2>&1; then
+  echo "JENKINS_SLAVE_SSH_PUBKEY=$(cat .ssh/id_rsa.pub)" >>./.env
+else
+  sed -i -e "s/^JENKINS_SLAVE_SSH_PUBKEY=.*/JENKINS_SLAVE_SSH_PUBKEY=$(cat .ssh/id_rsa.pub | sed "s/\//\\\\\//g")/g" ./.env
+fi
