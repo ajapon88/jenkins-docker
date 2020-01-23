80459e0b6763f952d827632056d0ad769ebc863e
ajapon88 <ajapon88@gmail.com>
2020-01-23 11:42:27 +0900

    rename script variable JENKINS_HOST to JENKINS_URL

--- scripts/jenkins-installed-plugins-list.sh
+++ scripts/jenkins-installed-plugins-list.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z "${JENKINS_HOST}" ]; then
-  # JENKINS_HOST=<username>:<password>@localhost:8080
-  JENKINS_HOST=localhost:8080
+if [ -z "${JENKINS_URL}" ]; then
+  # JENKINS_URL=http://<username>:<password>@localhost:8080
+  JENKINS_URL=http://localhost:8080
 fi
 
-curl -sSL "http://${JENKINS_HOST}/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g' | sed 's/ /:/' | sort
+curl --fail -sSL "${JENKINS_URL}/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g' | sed 's/ /:/' | sort
--- scripts/update-jnlp-secret.sh
+++ scripts/update-jnlp-secret.sh
@@ -2,14 +2,14 @@
 
 cd "$(dirname "${0}")/../"
 
-if [ -z "${JENKINS_HOST}" ]; then
-  # JENKINS_HOST=<username>:<password>@localhost:8080
-  JENKINS_HOST=localhost:8080
+if [ -z "${JENKINS_URL}" ]; then
+  # JENKINS_URL=http://<username>:<password>@localhost:8080
+  JENKINS_URL=http://localhost:8080
 fi
 if [ -z "${AGENT_NAME}" ]; then
   AGENT_NAME=jnlp-slave
 fi
 
-JENKINS_SECRET=$(curl -sSL "http://${JENKINS_HOST}/computer/${AGENT_NAME}/slave-agent.jnlp" | xmllint --xpath "//jnlp/application-desc /argument[1]/text()" -)
+JENKINS_SECRET=$(curl --fail -sSL "${JENKINS_URL}/computer/${AGENT_NAME}/slave-agent.jnlp" | xmllint --xpath "//jnlp/application-desc /argument[1]/text()" -)
 
 echo "JENKINS_SECRET=${JENKINS_SECRET}" >./jnlp-slave.env
